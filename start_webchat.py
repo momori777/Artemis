9a8cfc90adba92e0c5aba54dbb6e464bce1b58f7
momori777 <longyiwenzi991@gmail.com>
2026-06-26 01:45:50 +0800

    fix:hardcode start_webchat

--- start_webchat.py
+++ start_webchat.py
@@ -1,5 +1,6 @@
-import http.server, os
-os.chdir(r'D:\AI_Girlfriend\web-chat')
+import http.server, os, pathlib
+BASE_DIR = pathlib.Path(__file__).resolve().parent
+os.chdir(BASE_DIR / 'web-chat')
 print('CWD:', os.getcwd())
 server = http.server.HTTPServer(('127.0.0.1', 19270), http.server.SimpleHTTPRequestHandler)
 print('Webchat on :19270')
